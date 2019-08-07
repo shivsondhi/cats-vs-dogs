d4efc6597d6cadf03e9f63453c3954e0ea37692e
Shiv Sondhi <shiv.codes.22@gmail.com>
2019-08-07 14:05:35 +0530

    Added retraining mode

--- cats_and_dogs.py
+++ cats_and_dogs.py
@@ -26,9 +26,10 @@ from keras.callbacks import ModelCheckpoint
 import os
 
 def main():
-	modes = ['training', 'testing', 'none']
+	modes = ['training', 'retraining', 'testing', 'none']
 	mode = modes[1]
-	epoch_num = 5
+	epoch_num = 5		# total epochs to be done
+	epoch_done = 0		# number of epochs done already if retraining
 
 	print("We are in the {0} phase".format(mode), end="\n\n")
 
@@ -67,7 +68,7 @@ def main():
 						class_mode = 'binary')
 	print("training data batch generator created.")
 	test_set = test_datagen.flow_from_directory(
-						'dogs_vs_cats\\training_set',
+						'dogs_vs_cats\\test_set',
 						target_size = (64, 64),
 						batch_size = 32,
 						class_mode = 'binary')
@@ -89,6 +90,29 @@ def main():
 						validation_steps = 800,
 						verbose = 1,
 						callbacks = callbacks_list)
+		print("\n\t\t~Fin~\n")
+	elif mode == "retraining":
+		epoch_num -= epoch_done
+		# load the previous best weights manually
+		best_file = "weightsFile1\\first_weights-01-0.4064-0.74.hdf5"
+		classifier.load_weights(best_file)
+		classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+		print("Model loaded and compiled!", end="\n\n")
+		# name of file to save as 
+		filepath = ("weightsFile1\\first_weights-contd_%d-{epoch:02d}-{loss:.4f}-{val_acc:.2f}.hdf5", epoch_done)
+		#create model checkpoint
+		checkpoint = ModelCheckpoint(filepath, monitor='loss', save_best_only=True)
+		callbacks_list = [checkpoint] 
+		# fit the model
+		print("Starting training...")
+		classifier.fit_generator(training_set,
+					steps_per_epoch = 7000,
+					epochs = epoch_num,
+					validation_data = test_set,
+					validation_steps = 800,
+					verbose = 1,
+					callbacks = callbacks_list
+					)
 		print("\n\t\t~Fin~\n")
 	elif mode == "testing":
 		#load model weights
