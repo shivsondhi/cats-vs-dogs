c4b5c228f55ff0b841cd372d88ae08c8a7d67983
Shiv Sondhi <shiv.codes.22@gmail.com>
2019-08-07 19:58:37 +0530

    Removing old weight files

--- cats_and_dogs.py
+++ cats_and_dogs.py
@@ -1,21 +1,21 @@
 '''
 Cats and Dogs - Image Classification Task using Python, Keras and TensorFlow.
 
-TRIAL 1 :	7000 steps / epoch
-			epoch = 1;		loss 	 = 0.4053;		acc 	= 0.8081
-			time: 1885s;	val_loss = 0.1738;		val_acc = 0.9318
+TRIAL 1 :	125 steps / epoch, with batch size = 32
+			epoch = 1; 		loss 	 = 0.7071;		acc 	= 0.5707
+			time: 231s; 	val_loss = 0.6337; 		val_acc = 0.6512
 
-			epoch = 2;		loss 	 = 0.163;		acc 	= 0.9358
-			time: 1602s;	val_loss = 0.067;		val_acc = 0.9780
+			epoch = 5; 		loss 	 = 0.5774; 		acc 	= 0.7010
+			time: 105s; 	val_loss = 0.6002; 		val_acc = 0.6836
 
-			epoch = 3;		loss 	 = 0.0846;		acc 	= 0.9693
-			time: 1128s;	val_loss = 0.0396;		val_acc = 0.9856
+			epoch = 10; 	loss 	 = 0.5355; 		acc 	= 0.5259
+			time: 164s; 	val_loss = 0.5259; 		val_acc = 0.7364
 
-			epoch = 4; 		loss 	 = 0.0593;		acc 	= 0.9795
-			time: 1126s;	val_loss = 0.0305;		val_acc = 0.9891
+			epoch = 15; 	loss 	 = 0.5025; 		acc 	= 0.7575
+			time: 122s; 	val_loss = 0.5756; 		val_acc = 0.7208
 
-			epoch = 5; 		loss 	 = 0.0456; 		acc 	= 0.9844
-			time: 1098s; 	val_loss = 0.0399; 		val_acc = 0.9861
+			epoch = 20; 	loss 	 = 0.4890; 		acc 	= 0.7618
+			time: 153s; 	val_loss = 0.5119; 		val_acc = 0.7549
 '''
 
 import numpy as np
@@ -28,7 +28,7 @@ import os
 def main():
 	modes = ['training', 'retraining', 'testing', 'none']
 	mode = modes[1]
-	epoch_num = 5		# total epochs to be done
+	epoch_num = 20		# total epochs to be done
 	epoch_done = 0		# number of epochs done already if retraining
 
 	print("We are in the {0} phase".format(mode), end="\n\n")
@@ -70,7 +70,6 @@ def main():
 	test_set = test_datagen.flow_from_directory(
 						'dogs_vs_cats\\test_set',
 						target_size = (64, 64),
-						batch_size = 32,
 						class_mode = 'binary')
 	print("test data batch generator created.", end="\n\n")
 
@@ -78,23 +77,24 @@ def main():
 		#name of file to save as
 		filepath = "weightsFile1\\first_weights-{epoch:02d}-{loss:.4f}-{val_acc:.2f}.hdf5"
 		#create a checkpoint to callback at after every epoch
-		checkpoint = ModelCheckpoint(filepath, period=1)
+		checkpoint = ModelCheckpoint(filepath, monitor='loss', save_best_only=True)
 		callbacks_list = [checkpoint]
 		#fit the model
 		print("Starting training...")
 		classifier.fit_generator(
 						training_set,
-						steps_per_epoch = 7000,
+						steps_per_epoch = 125,
 						epochs = epoch_num,
 						validation_data = test_set,
-						validation_steps = 800,
+						validation_steps = 1000,
 						verbose = 1,
 						callbacks = callbacks_list)
 		print("\n\t\t~Fin~\n")
+	
 	elif mode == "retraining":
 		epoch_num -= epoch_done
 		# load the previous best weights manually
-		best_file = "weightsFile1\\first_weights-01-0.4064-0.74.hdf5"
+		best_file = "weightsFile1\\first_weights-contd_10-07-0.4837-0.73.hdf5"
 		classifier.load_weights(best_file)
 		classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 		print("Model loaded and compiled!", end="\n\n")
@@ -106,17 +106,18 @@ def main():
 		# fit the model
 		print("Starting training...")
 		classifier.fit_generator(training_set,
-					steps_per_epoch = 7000,
+					steps_per_epoch = 125,
 					epochs = epoch_num,
 					validation_data = test_set,
-					validation_steps = 800,
+					validation_steps = 1000,
 					verbose = 1,
 					callbacks = callbacks_list
 					)
 		print("\n\t\t~Fin~\n")
+	
 	elif mode == "testing":
 		#load model weights
-		best_file = "weightsFile1\\first_weights-05-0.0456-0.99.hdf5"
+		best_file = "weightsFile1\\first_weights-contd_10-07-0.4837-0.73.hdf5"
 		classifier.load_weights(best_file)
 		classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 		print("Model loaded and compiled!", end="\n\n")
